b59909b97ff326ca9c5c41a01b1e1246a2024775
LucasBortoloti <bortoloti91@gmail.com>
2024-03-04 11:20:51 -0300

    04/03

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+$options = new Options();
+$options->setChroot(__DIR__);
+
+$dompdf = new Dompdf($options);
+
+$dompdf->loadHtmlFile(__DIR__.'/tiny.html');
+
+$dompdf->render();
+
+header('Content-type: application/pdf');
+
+echo $dompdf->output();
