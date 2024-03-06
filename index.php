913654cd523fad5f5c890c6847b3223477167392
LucasBortoloti <bortoloti91@gmail.com>
2024-03-06 11:25:58 -0300

    06/03

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
 $options = new Options();
 $options->setChroot(__DIR__);
+$options->setIsRemoteEnabled(true);
 
 $dompdf = new Dompdf($options);
 
-$dompdf->loadHtmlFile(__DIR__.'/tiny.html');
+$dompdf->loadHtmlFile(__DIR__ . '/teste.html');
 
 $dompdf->render();
 
