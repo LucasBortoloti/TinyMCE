4eb93760d04b8aa3c8ddb0f7713a28aa17c0788f
LucasBortoloti <bortoloti91@gmail.com>
2024-03-14 09:22:00 -0300

    14/03

--- index.php
+++ index.php
@@ -11,14 +11,14 @@ $options->setIsRemoteEnabled(true);
 
 $dompdf = new Dompdf($options);
 
-$dompdf->loadHtmlFile(__DIR__ . '/teste.html');
+$dompdf->loadHtmlFile(__DIR__ . '/testeheader.html');
 
 $dompdf->render();
 
-$totalPages = $dompdf->get_canvas()->get_page_count();
+//$totalPages = $dompdf->get_canvas()->get_page_count();
 
-$canvas = $dompdf->get_canvas();
-$font = $dompdf->getFontMetrics()->get_font("Arial", "normal");
-$canvas->page_text(540, 800, "{PAGE_NUM} de $totalPages", $font, 10);
+//$canvas = $dompdf->get_canvas();
+//$font = $dompdf->getFontMetrics()->get_font("Arial", "normal");
+//$canvas->page_text(540, 800, "{PAGE_NUM} de $totalPages", $font, 10);
 
 $dompdf->stream('documento.pdf', array('Attachment' => 0));
